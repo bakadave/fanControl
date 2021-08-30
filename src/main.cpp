bf0d631442e8c8bbf6188d4b7eabff71db9758cc
bakadave@hotmail.com <bakadave@hotmail.com>
2021-08-30 20:26:42 +0200

    added PID control

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <TaskScheduler.h>
-//#include <ModbusRTU.h>
 #include <ModbusIP_ESP8266.h>
 #include "secrets.h"
 
@@ -22,9 +21,12 @@
 #define ST(A) #A
 #define STR(A) ST(A)
 
+#define LED_ON 0x00
+#define LED_OFF 0x01
+
 constexpr uint32_t BAUDRATE = 9600;
 constexpr uint32_t PWMfreq = 25000;
-constexpr uint8_t rpmPin = D1; //D1
+constexpr uint8_t rpmPin = D1;
 constexpr uint8_t pwmPin = D3;
 constexpr uint8_t onewireBUS = D4;
 constexpr uint8_t ledPin = 2;
@@ -36,12 +38,11 @@ constexpr char mDNS_name[] = STR(MDNS_NAME);
 const uint16_t port = atoi(STR(PORT));
 
 volatile uint8_t halfRevs = 0;
-uint8_t lowRPMoverflow = 0;
-uint8_t pwm = 255;
+uint16_t pwm = 1023;
+uint16_t setPoint_RPM = 0;
 DeviceAddress tempAddr;
 
 struct Kalman {
-
     private:
     float Pc = 0.0;
     float G = 0.0;
@@ -64,7 +65,7 @@ struct Kalman {
 
         return Xe;
     }
-} RPMfilter;
+};
 
 struct PIDcontroller {
     private:
@@ -74,19 +75,14 @@ struct PIDcontroller {
 
     public:
     unsigned int Kp = 1;
-    unsigned int Ki = 1;
-    unsigned int Kd = 1;
+    unsigned int Kd = 0.1;
+    unsigned int Ki = 1e-2;
 
     void init (uint16_t _setPoint) {
         setPoint = &_setPoint;
     }
 
-    uint16_t compute(uint16_t input) {
-        unsigned long currentTime = millis();
-
-        //handle overlfow
-        unsigned int elapsedTime = (previousTime > currentTime) ? (ULONG_MAX - previousTime) + currentTime : currentTime - previousTime;
-
+    uint16_t compute(uint16_t input, unsigned int elapsedTime) {
         int error = *setPoint - input;
         cumError += error * elapsedTime;
         int rateError = (error - lastError) / elapsedTime;
@@ -94,11 +90,10 @@ struct PIDcontroller {
         uint16_t output = Kp * error + Ki + cumError + Kd * rateError;
 
         lastError = error;
-        previousTime = currentTime;
 
         return output;
     }
-} pid;
+};
 
 IRAM_ATTR void rpmISR();
 void calculateRPM_callback();
@@ -109,8 +104,9 @@ void setup_OTA();
 
 ESP8266WiFiMulti wifiMulti;
 ModbusIP modbus;
-//ModbusRTU modbus;
 Scheduler ts;
+struct Kalman RPMfilter;
+struct PIDcontroller pid;
 Task calculateRPM(RPMcalcPeriodMS, TASK_FOREVER, &calculateRPM_callback);
 Task measureTemp(750, TASK_FOREVER, &measureTemp_callback);
 Task measureVoltages(500, TASK_FOREVER, &measureVoltages_callback);
@@ -119,14 +115,14 @@ DallasTemperature ds(&oneWire);
 
 void setup() {
     pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin, LED_ON);
 
     Serial.begin(9600);
 
     setup_wifi(APlist, APlen);
     setup_OTA();
     MDNS.begin(mDNS_name);
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin, LED_OFF);
 
     modbus.server();
     for (size_t i = 0; i < numRegs; i++)
@@ -139,6 +135,10 @@ void setup() {
     modbus.Hreg(3, pwm);
 
     attachInterrupt(digitalPinToInterrupt(rpmPin), rpmISR, CHANGE);
+
+    //init PID controller for fan RPM
+    pid.init(setPoint_RPM);
+    modbus.Hreg(0, setPoint_RPM);
 
     ts.init();
     ts.addTask(calculateRPM);
@@ -159,15 +159,11 @@ void setup() {
 void loop() {
     ts.execute();
 
-    pwm = modbus.Hreg(3);
-    analogWrite(pwmPin, pwm);
-
-    //! enable for continuous reconnect if wifi is lost
     wifiMulti.run();
     ArduinoOTA.handle();
 
-    modbus.Hreg(0, WiFi.status());
     modbus.task();
+    setPoint_RPM = modbus.Hreg(0);
     yield();
 }
 
@@ -182,16 +178,21 @@ void measureTemp_callback() {
 }
 
 void calculateRPM_callback() {
-    float rpm;
-    uint16_t deltaT = RPMcalcPeriodMS + calculateRPM.getStartDelay();
-    modbus.Hreg(6, deltaT);
-    modbus.Hreg(7, halfRevs);
-
-    rpm = (halfRevs / ((float)deltaT / 1000.0)) * 60.0 / 4;
+    uint8_t _revs = halfRevs;
     halfRevs = 0;
+    float rpm, rpm_f;   //shouldn't be float
+    uint16_t deltaT = RPMcalcPeriodMS + calculateRPM.getStartDelay();
 
-    modbus.Hreg(1, RPMfilter.addMeasurement(rpm));
+    modbus.Hreg(6, deltaT);
+    modbus.Hreg(7, _revs);
+
+    rpm = (_revs / ((float)deltaT / 1000.0)) * 60.0 / 4;
+    rpm_f = RPMfilter.addMeasurement(rpm);
+    pwm = pid.compute(rpm_f, deltaT);
+
+    modbus.Hreg(1, rpm_f);
     modbus.Hreg(2, rpm);
+    modbus.Hreg(3, pwm);
     Serial.printf("Measured RPM: %u Filtered RPM: %u\r\n", (uint16_t)rpm, modbus.Hreg(1));
 }
 
